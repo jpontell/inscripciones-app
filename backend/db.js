2dd72b6a57778a82a067f66e548a69b24794b549
jpontell <jpontell@hotmail.com>
2026-02-20 12:15:58 -0300

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -1,1 +1,12 @@
+const { Pool } = require("pg");
 
+const pool = new Pool({
+  host: "TU_HOST_RENDER",        // Host que te da Render
+  user: "TU_USUARIO_RENDER",
+  password: "TU_PASSWORD_RENDER",
+  database: "NOMBRE_DB_RENDER",
+  port: 5432,
+  ssl: { rejectUnauthorized: false } // obligatorio en Render
+});
+
+module.exports = pool;
